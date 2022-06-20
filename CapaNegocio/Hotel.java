a4a0078105544a42a20e137b56b9d34ce5fdbdd5
SMark665 <101886509+SMark665@users.noreply.github.com>
2022-06-20 16:40:50 -0500

    Update Hotel.java

--- CapaNegocio/Hotel.java
+++ CapaNegocio/Hotel.java
@@ -4,10 +4,27 @@
  */
 package CapaNegocio;
 
+import java.util.ArrayList;
 /**
  *
  * @author Mark
  */
 public class Hotel {
+    private int cantHabitaciones;
+
+    public int getCantHabitaciones() {
+        return cantHabitaciones;
+    }
+
+    public void setCantHabitaciones(int cantHabitaciones) {
+        this.cantHabitaciones = cantHabitaciones;
+    }
     
+    public void Hospedar(){}
+    
+    public ArrayList<Agencia> arreglo = new ArrayList();
+    
+    public void agregarAgencia(Agencia agencia){
+        arreglo.add(agencia);
+    }
 }
