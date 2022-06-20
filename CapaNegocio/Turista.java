95798e201f519ffa61e3e6f325fb2fd744502fad
SMark665 <101886509+SMark665@users.noreply.github.com>
2022-06-20 16:43:19 -0500

    Update Turista.java

--- CapaNegocio/Turista.java
+++ CapaNegocio/Turista.java
@@ -4,10 +4,52 @@
  */
 package CapaNegocio;
 
+import java.util.ArrayList;
 /**
  *
  * @author Mark
  */
 public class Turista {
+    private String Nombres;
+    private String documentoIden;
+    private int edad;
+    private String lugarOrigen;
+
+    public String getNombres() {
+        return Nombres;
+    }
+
+    public void setNombres(String Nombres) {
+        this.Nombres = Nombres;
+    }
+
+    public String getDocumentoIden() {
+        return documentoIden;
+    }
+
+    public void setDocumentoIden(String documentoIden) {
+        this.documentoIden = documentoIden;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLugarOrigen() {
+        return lugarOrigen;
+    }
+
+    public void setLugarOrigen(String lugarOrigen) {
+        this.lugarOrigen = lugarOrigen;
+    }
     
+    public void Aprender(){}
+    public void Experimentar(){}
+    
+    public Grupo esta;
+    public ArrayList<Reserva> posee = new ArrayList();
 }
