fa29f92e18feef7756c16d0d6187010b17b927e7
SMark665 <101886509+SMark665@users.noreply.github.com>
2022-06-20 16:39:35 -0500

    Update Grupo.java

--- CapaNegocio/Grupo.java
+++ CapaNegocio/Grupo.java
@@ -4,10 +4,33 @@
  */
 package CapaNegocio;
 
+import java.util.ArrayList;
 /**
  *
  * @author Mark
  */
 public class Grupo {
+    private int nroGrupo;
+    private int cantTuristas;
+
+    public int getNroGrupo() {
+        return nroGrupo;
+    }
+
+    public void setNroGrupo(int nroGrupo) {
+        this.nroGrupo = nroGrupo;
+    }
+
+    public int getCantTuristas() {
+        return cantTuristas;
+    }
+
+    public void setCantTuristas(int cantTuristas) {
+        this.cantTuristas = cantTuristas;
+    }
+    public void Organizar(){}
     
+    public Guia aCargoDe;
+    
+    public ArrayList<Turista> esta = new ArrayList();
 }
