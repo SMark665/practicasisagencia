162d77299ed7f180ef603d8a1f2b8d7cd0e629ea
SMark665 <101886509+SMark665@users.noreply.github.com>
2022-06-20 16:37:43 -0500

    Update Agencia.java

--- CapaNegocio/Agencia.java
+++ CapaNegocio/Agencia.java
@@ -9,5 +9,49 @@ package CapaNegocio;
  * @author Mark
  */
 public class Agencia {
+    private String nombre;
+    private int cantSucursales;
+    private String Calificacion;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantSucursales() {
+        return cantSucursales;
+    }
+
+    public void setCantSucursales(int cantSucursales) {
+        this.cantSucursales = cantSucursales;
+    }
+
+    public String getCalificacion() {
+        return Calificacion;
+    }
+
+    public void setCalificacion(String Calificacion) {
+        this.Calificacion = Calificacion;
+    }
     
+    public void Planear(){}
+    public void Atender(){}
+    
+    
+    public ArrayList<Hotel> arreglo = new ArrayList();
+    
+    public ArrayList<Tour> contiene = new ArrayList();
+    
+    public void agregarHotel(Hotel hotel){
+        arreglo.add(hotel);
+    }
+    
+    public ArrayList<ServicioTransporte> asociado = new ArrayList();
+    
+    public void agregarServicioTransporte(ServicioTransporte transporte){
+        asociado.add(transporte);
+    }
 }
