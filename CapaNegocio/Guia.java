834eeb7e46979c92b597c6a61d4cf8564d03349a
SMark665 <101886509+SMark665@users.noreply.github.com>
2022-06-20 16:40:16 -0500

    Update Guia.java

--- CapaNegocio/Guia.java
+++ CapaNegocio/Guia.java
@@ -4,10 +4,36 @@
  */
 package CapaNegocio;
 
+import java.util.ArrayList;
 /**
  *
  * @author Mark
  */
 public class Guia {
+    private String nombres;
+    private String experiencia;
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(String experiencia) {
+        this.experiencia = experiencia;
+    }
     
+    public void Gestionar(){}
+    public void Guiar(){}
+    public void Enseñar(){}
+    
+    public Tour gestionado;
+    
+    public ArrayList<Grupo> aCargoDe = new ArrayList();
 }
